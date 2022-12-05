44dc01612df8ea3cd72b0bf64837296658ef15b0
dshivrin <dshivrin@gmail.com>
2022-12-05 19:26:32 +0200

    Added loading gif

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -8,12 +8,14 @@ import ConfirmModal from "../confirmModal/confirmModal";
 import AddNewContactBtn from "../shared/AddNewContact";
 import { fetchAllContacts } from "../shared/apiService";
 import ZeroState from "./zeroState";
+import Loading from "../loading/loading";
 import pen from "../../media/pen.svg";
 import reload from "../../media/arrow-clockwise.svg";
 import "./list.css";
 
 const List = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [selectedContact, setSelectedContact] = useState({} as Contact);
@@ -25,11 +27,13 @@ const List = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const result = await fetchAllContacts();
       setData(result.data);
+      setIsLoading(false);
     };
     fetchData();
-  }, [data]);
+  }, []);
 
   const onEditClick = (contact: Contact) => {
     setSelectedContact(contact);
@@ -55,8 +59,7 @@ const List = () => {
     setShowEditModal(false);
   };
 
-  //add loading gif before
-  if (data.length === 0) {
+  if (!isLoading && data.length === 0) {
     return (
       <div>
         <ZeroState action={onAddNew} />
@@ -69,82 +72,86 @@ const List = () => {
       </div>
     );
   } else {
-    //export this code to anothe component
     return (
       <div className="table-container">
-        <div className="header">
-          <AddNewContactBtn action={onAddNew} text="Add New Contact" />
-          <Button onClick={fetchData}>
-            <span className="btn-label-right">
-              <img className="invert" src={reload} />
-              <span>Refresh</span>
-            </span>
-          </Button>
-        </div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Social Number</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Birth Date</th>
-              <th>Gender</th>
-              <th>Phone Number</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((contact: Contact) => {
-              return (
-                <tr key={contact.id}>
-                  <td>{contact.id}</td>
-                  <td>{contact.socialNumber}</td>
-                  <td>{contact.name}</td>
-                  <td>{contact.email}</td>
-                  <td>{FormatDateString(contact.birthDate)}</td>
-                  <td>{contact.gender}</td>
-                  <td>{contact.phone}</td>
-                  <td>
-                    <div className="actions-container">
-                      <Button
-                        variant="info"
-                        onClick={() => onEditClick(contact)}
-                      >
-                        <span className="btn-label-left">
-                          <img src={pen} />
-                        </span>
-                        Edit
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => onDeleteClick(contact)}
-                      >
-                        <span className="btn-label-left">
-                          <i className="fa fa-trash"></i>
-                        </span>
-                        Delete
-                      </Button>
-                    </div>
-                  </td>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            <div className="header">
+              <AddNewContactBtn action={onAddNew} text="Add New Contact" />
+              <Button onClick={fetchData}>
+                <span className="btn-label-right">
+                  <img className="invert" src={reload} />
+                  <span>Refresh</span>
+                </span>
+              </Button>
+            </div>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Social Number</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Birth Date</th>
+                  <th>Gender</th>
+                  <th>Phone Number</th>
+                  <th>Actions</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-        {/* Modals */}
-        <EditModal
-          show={showEditModal}
-          closeModal={CloseEditModal}
-          contactId={selectedContact.id}
-          reload={fetchData}
-        />
-        <ConfirmModal
-          show={showDeleteModal}
-          setShow={setShowDeleteModal}
-          contact={selectedContact}
-          setContact={updateContactState}
-        />
+              </thead>
+              <tbody>
+                {data.map((contact: Contact) => {
+                  return (
+                    <tr key={contact.id}>
+                      <td>{contact.id}</td>
+                      <td>{contact.socialNumber}</td>
+                      <td>{contact.name}</td>
+                      <td>{contact.email}</td>
+                      <td>{FormatDateString(contact.birthDate)}</td>
+                      <td>{contact.gender}</td>
+                      <td>{contact.phone}</td>
+                      <td>
+                        <div className="actions-container">
+                          <Button
+                            variant="info"
+                            onClick={() => onEditClick(contact)}
+                          >
+                            <span className="btn-label-left">
+                              <img src={pen} />
+                            </span>
+                            Edit
+                          </Button>
+                          <Button
+                            variant="danger"
+                            onClick={() => onDeleteClick(contact)}
+                          >
+                            <span className="btn-label-left">
+                              <i className="fa fa-trash"></i>
+                            </span>
+                            Delete
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+            <EditModal
+              show={showEditModal}
+              closeModal={CloseEditModal}
+              contactId={selectedContact.id}
+              reload={fetchData}
+            />
+            <ConfirmModal
+              show={showDeleteModal}
+              setShow={setShowDeleteModal}
+              contact={selectedContact}
+              setContact={updateContactState}
+            />
+          </div>
+        )}
       </div>
     );
   }
--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,7 +1,12 @@
+import './loading.css';
 
 
 const Loading = () => {
-
-}
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
 
 export default Loading;
